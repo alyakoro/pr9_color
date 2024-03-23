498dfff8417e5621d6eafbfd7c52f3cc601ae811
1101-22 <alya.korobeynikova99@gmail.com>
2024-03-23 14:15:49 +0300

    0.2

--- app/src/main/java/comkorobeynikova/pr9_color/MainActivity.kt
+++ app/src/main/java/comkorobeynikova/pr9_color/MainActivity.kt
@@ -1,13 +1,16 @@
 package comkorobeynikova.pr9_color
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import comkorobeynikova.pr9_color.databinding.ActivityMainBinding
+import comkorobeynikova.pr9_color.databinding.ColorpickBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingv: ColorpickBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +32,14 @@ class MainActivity : AppCompatActivity() {
 
         val dialog = dialogBuilder.show()
 
-        dialogView.buttonOk.setOnClickListen
+        bindingv.ok.setOnClickListener {
+            val color = Color.rgb(
+                bindingv.R.text.toString().toInt(),
+                bindingv.G.text.toString().toInt(),
+                bindingv.B.text.toString().toInt()
+            )
+            binding.colorBg.setBackgroundColor(color)
+            dialog.dismiss()
+        }
     }
 }
