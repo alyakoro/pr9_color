3b7be2dcbf3815ca18636e5ebb3cacdc600c81e7
1101-22 <alya.korobeynikova99@gmail.com>
2024-03-30 09:57:16 +0300

    0.8

--- app/src/main/java/comkorobeynikova/pr9_color/MainActivity.kt
+++ app/src/main/java/comkorobeynikova/pr9_color/MainActivity.kt
@@ -3,6 +3,7 @@ package comkorobeynikova.pr9_color
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AlertDialog
 import comkorobeynikova.pr9_color.databinding.ActivityMainBinding
 import comkorobeynikova.pr9_color.databinding.ColorpickBinding
@@ -10,13 +11,16 @@ import comkorobeynikova.pr9_color.databinding.ColorpickBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bindingv: ColorpickBinding
+    private lateinit var bindings: ColorpickBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindings = ColorpickBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.colorbutton.setOnClickListener {
@@ -32,13 +36,23 @@ class MainActivity : AppCompatActivity() {
 
         val dialog = dialogBuilder.show()
 
-        bindingv.ok.setOnClickListener {
+        bindings.RED.setOnSeekBarChangeListener (object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                bindings.RR.setText(progress)
+            }
+        }
+
+        bindings.ok.setOnClickListener {
             val color = Color.rgb(
-                bindingv.RR.text.toString().toInt(),
-                bindingv.G.text.toString().toInt(),
-                bindingv.B.text.toString().toInt()
+                bindings.RR.text.toString().toInt(),
+                bindings.G.text.toString().toInt(),
+                bindings.B.text.toString().toInt()
             )
             binding.colorBg.setBackgroundColor(color)
+            dialog.dismiss()
+        }
+
+        bindings.cancel.setOnClickListener {
             dialog.dismiss()
         }
     }
